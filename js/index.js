e2589c94fa4e2e704484fad5f0c08c5f4d1990b4
Nickothan <s1nicolasrg@gmail.com>
2021-09-03 14:34:09 -0500

    implement promises for xhttp request

--- js/index.js
+++ js/index.js
@@ -2,35 +2,49 @@ const CITY_NAME = 'Bogota'
 
 const API_KEY = 'c7b130ee1874590f4e7828eab0fbf4fd'
 
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`
+const API_DAY = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`
 
-const API_CALL_DAYS = `https://api.openweathermap.org/data/2.5/forecast?q=${CITY_NAME}&appid=${API_KEY}`
+const API_DAYS = `https://api.openweathermap.org/data/2.5/forecast?q=${CITY_NAME}&appid=${API_KEY}`
 
 let nameCityActual = document.getElementById('city-actual-name')
 let weatherCityActual = document.getElementById('weather-acity-actual')
 let temperatureCityActual = document.getElementById('temperature-city-actual')
 
-function ajax(url, callback) {
-  const xhttp = new XMLHttpRequest()
-  xhttp.onload = function () {
-    callback(JSON.parse(this.responseText))
-  }
-  xhttp.open('GET', url)
-  xhttp.send()
+function ajax(url) {
+  return new Promise(function (resolve, reject) {
+    const xhttp = new XMLHttpRequest()
+    xhttp.open('GET', url)
+    xhttp.onload = function () {
+      if (this.status >= 200 && this.status < 300)
+        resolve(JSON.parse(this.responseText))
+      reject(this)
+    }
+    xhttp.onerror = function () {
+      reject(this)
+    }
+    xhttp.send()
+  })
 }
 
 async function loadInfoDayActual() {
-  const actualDay = await ajax(API_URL, loadInfoDayActual)
-  console.log(':: Day : ', actualDay)
-  /*   nameCityActual.innerHTML = actualDay.name
-  weatherCityActual.innerHTML = actualDay.weather[0].main
-  temperatureCityActual.innerHTML = Math.trunc(actualDay.main.temp - 273.15) */
+  try {
+    const actualDay = await ajax(API_DAY, loadInfoDayActual)
+    console.log(':: Day : ', actualDay)
+    /*   nameCityActual.innerHTML = actualDay.name
+    weatherCityActual.innerHTML = actualDay.weather[0].main
+    temperatureCityActual.innerHTML = Math.trunc(actualDay.main.temp - 273.15) */
+  } catch (error) {
+    console.error(':: error ', error)
+  }
 }
 
 async function loadInfoCallDays() {
-  const days = await ajax(API_URL, loadInfoDayActual)
-
-  console.log(' :: resultado : ', days)
+  try {
+    const days = await ajax(API_DAYS, loadInfoDayActual)
+    console.log(' :: Days ', days)
+  } catch (error) {
+    console.log(' :: Error ', error)
+  }
 }
 
 loadInfoDayActual()
