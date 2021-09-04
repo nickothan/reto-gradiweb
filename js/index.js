d8c85150262096b0321d93f7e778b7d3cb2d1e1e
Nickothan <s1nicolasrg@gmail.com>
2021-09-03 21:38:21 -0500

    implement data in 3 days forecast

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 const CITY_NAME = 'Bogota'
 
-const API_KEY = 'c7b130ee1874590f4e7828eab0fbf4fd'
+const API_KEY = '35f44bee6f329fca8fb84e86f7842faa'
 
 const API_DAY = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`
 
 const API_DAYS = `https://api.openweathermap.org/data/2.5/forecast?q=${CITY_NAME}&appid=${API_KEY}`
 
+//? Día actual
+
 let nameCityActual = document.getElementById('city-actual-name')
 let weatherCityActual = document.getElementById('weather-acity-actual')
 let temperatureCityActual = document.getElementById('temperature-city-actual')
+
+//? Pronostico de dias
+//? dia 1
+
+let forecast1 = document.getElementById('forecastDay1')
+
+//? dia 2
+
+//? dia 3
 
 function ajax(url) {
   return new Promise(function (resolve, reject) {
@@ -29,10 +40,10 @@ function ajax(url) {
 async function loadInfoDayActual() {
   try {
     const actualDay = await ajax(API_DAY, loadInfoDayActual)
-    console.log(':: Day : ', actualDay)
-    /*   nameCityActual.innerHTML = actualDay.name
+    //console.log(':: Day : ', actualDay)
+    nameCityActual.innerHTML = actualDay.name
     weatherCityActual.innerHTML = actualDay.weather[0].main
-    temperatureCityActual.innerHTML = Math.trunc(actualDay.main.temp - 273.15) */
+    temperatureCityActual.innerHTML = Math.trunc(actualDay.main.temp - 273.15)
   } catch (error) {
     console.error(':: error ', error)
   }
@@ -41,9 +52,80 @@ async function loadInfoDayActual() {
 async function loadInfoCallDays() {
   try {
     const days = await ajax(API_DAYS, loadInfoDayActual)
-    console.log(' :: Days ', days)
+    let contador = 0
+    let contadordiario = 0
+
+    const nombreDelDiaSegunFecha = (fecha) =>
+      [
+        'sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ][new Date(fecha).getDay()]
+
+    let dia = ''
+
+    console.log(days.list)
+    days.list.map((props) => {
+      contador++
+
+      if (contador == 1) {
+        contadordiario++
+
+        //? dia de la semana
+        let forecast = document.getElementById(`forecastDay${contadordiario}`)
+        forecast.innerHTML = nombreDelDiaSegunFecha(props.dt_txt)
+
+        //? estado del clima
+        let forecastWeather = document.getElementById(
+          `forecastWeather${contadordiario}`
+        )
+        forecastWeather.innerHTML = props.weather[0].description
+
+        //? Temperatura maxima y minima
+        let tempMax = document.getElementById(
+          `forecastTempMax${contadordiario}`
+        )
+        tempMax.innerHTML = Math.trunc(props.main.temp_max - 273.15)
+        let tempMin = document.getElementById(
+          `forecastTempMin${contadordiario}`
+        )
+        tempMin.innerHTML = Math.trunc(props.main.temp_min - 273.15)
+
+        //? selector de imagenes
+        console.log(document.getElementById('forecastImgDay1').src)
+      } else if (contador % 9 == 0 && contador < 20) {
+        contadordiario++
+        //? dia de la semana
+        let forecast = document.getElementById(`forecastDay${contadordiario}`)
+        forecast.innerHTML = nombreDelDiaSegunFecha(props.dt_txt)
+
+        //? estado del clima
+        let forecastWeather = document.getElementById(
+          `forecastWeather${contadordiario}`
+        )
+        forecastWeather.innerHTML = props.weather[0].description
+
+        //? Temperatura maxima y minima
+        let tempMax = document.getElementById(
+          `forecastTempMax${contadordiario}`
+        )
+        tempMax.innerHTML = Math.trunc(props.main.temp_max - 273.15)
+        let tempMin = document.getElementById(
+          `forecastTempMin${contadordiario}`
+        )
+        tempMin.innerHTML = Math.trunc(props.main.temp_min - 273.15)
+      } else {
+      }
+      //
+    })
+
+    //console.log(' :: Days ', days.list)
   } catch (error) {
-    console.log(' :: Error ', error)
+    console.error(' :: Error ', error)
   }
 }
 
